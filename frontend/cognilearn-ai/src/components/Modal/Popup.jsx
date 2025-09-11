2a6af41b8808bbffd83738c0855d8e1934e97f7c
Nguyen Trong Hiep <hieppotato2@hotmail.com>
2025-09-12 00:22:00 +0700

    fix

--- frontend/cognilearn-ai/src/components/Modal/Popup.jsx
+++ frontend/cognilearn-ai/src/components/Modal/Popup.jsx
@@ -5,7 +5,7 @@ export function PopUpModal({ opened, onClose }) {
   return (
     <div className="lexend">
       <MantineModal opened={opened} onClose={onClose} title="Thành công!">
-        <Text>Quá trình đã hoàn tất và pop-up đã được hiển thị!</Text>
+        <Text>Bạn đã phù hợp để làm giáo viên rồi, không cần định hướng đâu!</Text>
         <Group position="right" mt="md">
           <Button onClick={onClose}>Đóng</Button>
         </Group>
